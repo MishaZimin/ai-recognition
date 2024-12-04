fd893ce2e3cf376738746a58324c14137ef17676
ZiminMisha <mikhail.zimin.2004@bk.ru>
2024-12-04 21:57:41 +0500

    fix ui

--- src/components/DigitRecognition.tsx
+++ src/components/DigitRecognition.tsx
@@ -14,10 +14,13 @@ const DigitRecognition: React.FC = () => {
     useEffect(() => {
         const loadModel = async () => {
             setLoading(true);
+
             await tf.ready();
             await tf.setBackend("webgl");
+
             const mobilenet = await import("@tensorflow-models/mobilenet");
             const model = await mobilenet.load();
+
             setModel(model);
             setLoading(false);
         };
@@ -50,8 +53,7 @@ const DigitRecognition: React.FC = () => {
                 </div>
             ) : (
                 <div className="relative flex flex-col justify-between h-full gap-6 my-auto sm:justify-normal">
-                    {/* Камера */}
-                    <div className="p-2 mt-0 sm:p-0 sm:mt-28">
+                    <div className="p-2 mt-0 sm:p-0 sm:mt-28 ">
                         <Webcam
                             audio={false}
                             ref={webcamRef}
@@ -60,10 +62,10 @@ const DigitRecognition: React.FC = () => {
                             videoConstraints={{
                                 facingMode: "environment",
                             }}
-                            className="w-full border rounded-[8px] mb-4"
+                            className="w-full rounded-[8px] mb-4"
                         />
 
-                        <p className="font-mono text-lg text-center mx-h-[28px]">
+                        <p className="font-mono text-lg text-center mx-h-[28px] mb-[64px]">
                             {prediction ? (
                                 <span className="text-green-500">
                                     {prediction}
@@ -73,10 +75,10 @@ const DigitRecognition: React.FC = () => {
                             )}
                         </p>
                     </div>
-                    <div className="absolute bottom-0 w-full p-2 sm:relative sm:p-0">
+                    <div className="fixed bottom-0 w-full p-2 sm:relative sm:p-0 ">
                         <button
                             onClick={captureFrame}
-                            className="px-4 py-3 bg-black bg-opacity-[4%] hover:bg-opacity-[8%] d rounded-[8px] w-full">
+                            className="px-4 py-3 bg-black bg-opacity-[4%] hover:bg-opacity-[8%] d rounded-[8px] w-full backdrop-blur-md">
                             <p className="font-mono font-semibold">
                                 Распознать
                             </p>
